d5f0b1b1ed7d887428cc4fa4bc1a67dc47eee138
ki4sooauth <845977162@qq.com>
2013-11-27 18:46:45 +0800

    d

--- gooagoo-authorize/gooagoo-authorize-service/src/main/java/com/gooagoo/authzserver/action/AuthzserverAction.java
+++ gooagoo-authorize/gooagoo-authorize-service/src/main/java/com/gooagoo/authzserver/action/AuthzserverAction.java
@@ -5,17 +5,8 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.oltu.oauth2.as.issuer.OAuthIssuer;
-import org.apache.oltu.oauth2.as.issuer.OAuthIssuerImpl;
-import org.apache.oltu.oauth2.as.issuer.UUIDValueGenerator;
-import org.apache.oltu.oauth2.as.request.OAuthAuthzRequest;
-import org.apache.oltu.oauth2.as.response.OAuthASResponse;
-import org.apache.oltu.oauth2.common.exception.OAuthProblemException;
-import org.apache.oltu.oauth2.common.message.OAuthResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.gooagoo.common.action.BaseAction;
 
@@ -31,42 +22,70 @@ public class AuthzserverAction extends BaseAction
      * @return
      * @throws IOException 
      */
-    @RequestMapping(method = RequestMethod.GET)
-    public ModelAndView requestService(HttpServletRequest request, HttpServletResponse response) throws Exception
+    @RequestMapping(params = " method = ologin")
+    public String requestCode(HttpServletRequest request, HttpServletResponse response) throws Exception
     {
-        try
-        {
 
-            /**
-              * 1.校验参数
-              * 2.页面授权
-              * 3.保存授权信息
-              * 4.
-              */
+        /* try
+         {
 
-            //dynamically recognize an OAuth profile based on request characteristic (params,
-            // method, content type etc.), perform validation
-            OAuthAuthzRequest oauthRequest = new OAuthAuthzRequest(request);
+             *//**
+               * 1.校验参数
+               * 2.页面授权
+               * 3.保存授权信息
+               * 4.
+               */
+        /*
 
-            //                        validateRedirectionURI(oauthRequest)
+        //dynamically recognize an OAuth profile based on request characteristic (params,
+        // method, content type etc.), perform validation
+        OAuthAuthzRequest oauthRequest = new OAuthAuthzRequest(request);
 
-            //build OAuth response
-            UUIDValueGenerator vg = new UUIDValueGenerator();
-            OAuthIssuer oauthIssuerImpl = new OAuthIssuerImpl(vg);
-            OAuthResponse resp = OAuthASResponse.authorizationResponse(request, HttpServletResponse.SC_FOUND).setCode(oauthIssuerImpl.authorizationCode()).location("").buildQueryMessage();
+        //                        validateRedirectionURI(oauthRequest)
 
-            response.sendRedirect(resp.getLocationUri());
+        //build OAuth response
+        UUIDValueGenerator vg = new UUIDValueGenerator();
+        OAuthIssuer oauthIssuerImpl = new OAuthIssuerImpl(vg);
+        OAuthResponse resp = OAuthASResponse.authorizationResponse(request, HttpServletResponse.SC_FOUND).setCode(oauthIssuerImpl.authorizationCode()).location("").buildQueryMessage();
 
-            //if something goes wrong
+        response.sendRedirect(resp.getLocationUri());
+
+        //if something goes wrong
         }
         catch (OAuthProblemException ex)
         {
-            final OAuthResponse resp = OAuthASResponse.errorResponse(HttpServletResponse.SC_FOUND).error(ex).location("").buildQueryMessage();
+        final OAuthResponse resp = OAuthASResponse.errorResponse(HttpServletResponse.SC_FOUND).error(ex).location("").buildQueryMessage();
 
-            response.sendRedirect(resp.getLocationUri());
+        response.sendRedirect(resp.getLocationUri());
         }
-
-        return new ModelAndView("index");
+        */
+        return "ologin";
     }
 
+    /**
+     * 接口服务入口
+     * @param request
+     * @param response
+     * @return
+     * @throws IOException 
+     */
+    @RequestMapping(params = "method=login")
+    public String login(HttpServletRequest request, HttpServletResponse response) throws Exception
+    {
+        return "login";
+    }
+
+    /**
+     * 接口服务入口
+     * @param request
+     * @param response
+     * @return
+     * @throws IOException 
+     */
+    @RequestMapping(params = "method=qlogin")
+    public String responseCode(HttpServletRequest request, HttpServletResponse response) throws Exception
+    {
+        return "qlogin";
+
+    }
 }
